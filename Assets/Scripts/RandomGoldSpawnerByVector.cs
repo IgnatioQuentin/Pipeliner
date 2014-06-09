80d32e0b51b7ea2796c0b49a74b2252d2d8aa68e
IgnatioQ <martin.k.bjorkman@gmail.com>
2014-06-10 01:59:56 +0200

    Sandbox added

---
+++ Assets/Scripts/RandomGoldSpawnerByVector.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomGoldSpawnerByVector : MonoBehaviour {
+
+	public GameObject gold;
+	public GameObject GoldSpawn;
+	public int spawnsPerSecond;
+	public float forceX;
+	public float forceY;
+	public int forceMag;
+
+	private int counter;
+	private int cutOff;
+
+	private float randomOffsetX;
+	private float randomOffsetY;
+
+	public GameObject terrain;
+	// Use this for initialization
+	void Start () {
+		cutOff = spawnsPerSecond * 60;
+		Random.Range(-3, 3);
+		randomOffsetX = Random.value;
+		randomOffsetY = Random.value;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(counter == 0){
+			GameObject spawn = (GameObject)Instantiate(gold, GoldSpawn.transform.position , Quaternion.identity);
+			spawn.rigidbody2D.AddForce(new Vector2(((forceX * forceMag) + randomOffsetX), ((forceY * forceMag) + randomOffsetY)));
+		}
+
+		counter++;
+
+		if(counter > cutOff){
+			counter = 0;
+		}
+	}
+}
