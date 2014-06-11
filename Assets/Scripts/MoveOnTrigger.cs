4376e1be9b1a606c26e4d86946f3a37b5e76e59f
Markus Palacios <Markus.Palacios@gmail.com>
2014-06-11 13:36:23 +0200

    Created assemblyLine

---
+++ Assets/Scripts/MoveOnTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveOnTrigger : MonoBehaviour {
+	public float moveForce;
+	public bool right;
+	public bool left;
+	private float newX;
+	// Use this for initialization
+	void Start () {
+		if (right && left)
+			left = !left;
+	}
+
+	void OnTriggerStay2D (Collider2D other) {
+		if (right && other.tag != "Kinematic") {
+			other.transform.Translate (moveForce * Time.deltaTime, 0, 0, Camera.main.transform);
+		}
+
+		if (left && other.tag != "Kinematic") {
+			other.transform.Translate (-moveForce * Time.deltaTime, 0, 0, Camera.main.transform);
+		}
+	}
+}
